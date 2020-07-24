4fc7d95894323c39c56dea463d3b4bc2aa3eb57e
fsbmat <fsbmat@gmail.com>
2020-07-24 14:55:07 -0300

    Add plots

--- Leiden.R
+++ Leiden.R
@@ -4,46 +4,49 @@ library("data.table")
 library("stringr")
 library("tidyverse")
 library("leaflet")
-library(readr)
+library("readr")
+library("plotly")
+library("htmlTable")
 #library(readxl)
 #data <- read_excel("LeidenRanking2019.xlsx", sheet = 2)
 #write.csv(data, "LeidenRanking2019.csv", row.names = FALSE)
 #rm(data)
-data <- fread("LeidenRanking2019.csv", encoding = "Latin-1")
-names(data)
-data$Per_Init <- str_sub(data$Period,1,4)
-data$Per_End <- str_sub(data$Period,6,9)
-#list_of_files <- list.files(path = ".", recursive = TRUE,
-#                            pattern = "\\.txt$", 
-#                            full.names = TRUE)
-LatLong <- fread("LatLongCountry.txt", header = FALSE, sep = "\t", dec = ".")
-names(LatLong) <- c("Siglas", "latitude", "longitude", "Country")
-data <- left_join(data, LatLong, by="Country")
-data <- data %>% select(c(1:2,89:91, 4, 87:88, 3, 5:86))
-names(data)
-#Funcao para remover acentos
-data$University <- abjutils::rm_accent(data$University)
-data$Country    <- abjutils::rm_accent(data$Country   )
-data$Field      <- abjutils::rm_accent(data$Field     )
-#Apagar Espacos antes e apos o texto
-data$University <- str_trim(data$University, side = c("both", "left", "right"))
-data$University <- str_squish(data$University)
-data$Country <- str_trim(data$Country, side = c("both", "left", "right"))
-data$Country <- str_squish(data$Country)
-data$Field   <- str_trim(data$Field, side = c("both", "left", "right"))
-data$Field   <- str_squish(data$Field)
-#Transformar tudo em letras maiusculas
-data$University <- str_to_upper(data$University)
-data$Country  <- str_to_upper(data$Country)
-data$Field   <- str_to_upper(data$Field)
-
-saveRDS(data, file="LeidenRanking.Rds")
-dados <- readRDS("LeidenRanking.Rds")
+# data <- fread("LeidenRanking2019.csv", encoding = "Latin-1")
+# names(data)
+# data$Per_Init <- str_sub(data$Period,1,4)
+# data$Per_End <- str_sub(data$Period,6,9)
+# #list_of_files <- list.files(path = ".", recursive = TRUE,
+# #                            pattern = "\\.txt$", 
+# #                            full.names = TRUE)
+# LatLong <- fread("LatLongCountry.txt", header = FALSE, sep = "\t", dec = ".")
+# names(LatLong) <- c("Siglas", "latitude", "longitude", "Country")
+# data <- left_join(data, LatLong, by="Country")
+# data <- data %>% select(c(1:2,89:91, 4, 87:88, 3, 5:86))
+# names(data)
+# #Funcao para remover acentos
+# data$University <- abjutils::rm_accent(data$University)
+# data$Country    <- abjutils::rm_accent(data$Country   )
+# data$Field      <- abjutils::rm_accent(data$Field     )
+# #Apagar Espacos antes e apos o texto
+# data$University <- str_trim(data$University, side = c("both", "left", "right"))
+# data$University <- str_squish(data$University)
+# data$Country <- str_trim(data$Country, side = c("both", "left", "right"))
+# data$Country <- str_squish(data$Country)
+# data$Field   <- str_trim(data$Field, side = c("both", "left", "right"))
+# data$Field   <- str_squish(data$Field)
+# #Transformar tudo em letras maiusculas
+# data$University <- str_to_upper(data$University)
+# data$Country  <- str_to_upper(data$Country)
+# data$Field   <- str_to_upper(data$Field)
+# 
+# saveRDS(data, file="LeidenRanking.Rds")
+data <- readRDS("LeidenRanking.Rds")
 
 brazil <- data %>% filter(Country=="BRAZIL")
 unique(data$Country)
 
 Nruniversidades <- data %>% group_by(Country, latitude, longitude) %>% summarise(NrUniv=n_distinct(University))
+df <-  data.frame(Cor = topo.colors(56, alpha = NULL), stringsAsFactors = FALSE)
 str(Nruniversidades)
 Nruniversidades$Country <- as.factor(Nruniversidades$Country)
 Nruniversidades$latitude <- as.numeric(Nruniversidades$latitude) 
@@ -54,20 +57,111 @@ Nruniversidades %>%
   leaflet() %>% 
   addTiles() %>% 
   addMarkers(lng = ~longitude, lat = ~latitude, popup = ~NrUniv,
-             clusterOptions = markerClusterOptions())
-
-library(leaflet)
-m = leaflet() %>% addTiles()
-m  # a map with the default OSM tile layer
-
-m = m %>% setView(-93.65, 42.0285, zoom = 17)
-m
-
-m %>% addPopups(-93.65, 42.0285, 'Here is the <b>Department of Statistics</b>, ISU')
+             clusterOptions = markerClusterOptions(maxClusterRadius = 15))
 
 
-data(quakes)
+Nruniversidades %>% 
+  leaflet() %>% 
+  addTiles() %>% 
+  addCircleMarkers(lng = ~longitude, lat = ~latitude, label = ~as.character(NrUniv),
+             labelOptions = labelOptions(noHide = T, direction = 'center', textOnly = T), 
+             color = ~as.factor(NrUniv))
 
-# Show first 20 rows from the `quakes` dataset
-leaflet(data = quakes[1:20,]) %>% addTiles() %>%
-  addMarkers(~long, ~lat, popup = ~as.character(mag), label = ~as.character(mag))
+Nruniversidades %>% 
+  leaflet() %>% 
+  addTiles() %>% 
+  addLabelOnlyMarkers(lng = ~longitude, lat = ~latitude, label = ~as.character(NrUniv), 
+                      labelOptions = labelOptions(noHide = T, direction = 'top', textOnly = F))
+
+Nruniversidades %>% 
+  leaflet() %>% addTiles() %>%
+  addPopups(~longitude, ~latitude, ~as.character(NrUniv), 
+            options = popupOptions(minWidth = 20, closeOnClick = FALSE, closeButton = FALSE))
+
+Nruniversidades %>% 
+  leaflet() %>% addTiles() %>% 
+  addCircleMarkers(lng = ~longitude, lat = ~latitude, 
+             weight = 10, color = "#03F", 
+             opacity = 0.5,
+             stroke = FALSE,
+             popup = ~ NrUniv)
+
+
+# library(leaflet)
+# m = leaflet() %>% addTiles()
+# m  # a map with the default OSM tile layer
+# 
+# m = m %>% setView(-93.65, 42.0285, zoom = 17)
+# m
+# 
+# m %>% addPopups(-93.65, 42.0285, 'Here is the <b>Department of Statistics</b>, ISU')
+
+
+science <- data %>% group_by(Field) %>% summarise(n())
+
+
+ImpactP <- data %>% group_by(Country, University) %>% 
+  summarise(Impacto=sum(impact_P)) %>% arrange(desc(Impacto))
+
+
+
+Nr <- data %>% group_by(Country, University, Per_Init, Per_End) %>% 
+  summarise(Impacto=sum(impact_P)) %>% arrange(desc(Impacto)) 
+head(Nr)
+
+plot1 <- brazil %>% 
+  filter(University=="UNIVERSIDADE FEDERAL DE VICOSA",
+         Period=="2014–2017", 
+         Frac_counting=="0") %>% 
+  ggplot(aes(Field, impact_P, fill=Field, label= round(impact_P, digits = 2), 
+             text=paste("Produção:",impact_P, "<br>", 
+                        "Período:", Period))) +
+  geom_col(aes(Field, impact_P), show.legend = FALSE) + 
+  xlab("Área Ciêntífica (2014-2017)") + ylab("Número de Publicações com Impacto") + 
+  geom_text(position = position_dodge(width = 0.9), vjust = -0.5) + theme_bw()
+ggplotly(plot1, tooltip = "text") %>% layout(showlegend = FALSE) %>% style(textposition = "top")
+
+
+plot2 <- brazil %>% 
+  filter(University=="UNIVERSIDADE FEDERAL DE VICOSA",
+         Period=="2014–2017", 
+         Frac_counting=="1") %>% 
+  ggplot(aes(Field, impact_P, fill=Field, label= round(impact_P, digits = 2), 
+             text=paste("Produção:",impact_P, "<br>", 
+                        "Período:", Period))) +
+  geom_col(aes(Field, impact_P), show.legend = FALSE) + 
+  xlab("Área Ciêntífica (2014-2017)") + ylab("Número de Publicações com Impacto") + 
+  geom_text(position = position_dodge(width = 0.9), vjust = -0.5) + theme_bw()
+ggplotly(plot2, tooltip = "text") %>% layout(showlegend = FALSE) %>% style(textposition = "top")
+
+
+plot3 <- brazil %>% 
+  filter(University=="UNIVERSIDADE FEDERAL DE VICOSA",
+         Field=="MATHEMATICS AND COMPUTER SCIENCE", 
+         Frac_counting=="0") %>% group_by(Period) %>% 
+  ggplot(aes(Period, impact_P, fill=Period, label= round(impact_P, digits = 2), 
+             text=paste("Produção:",impact_P, "<br>", 
+                        "Período:", Period))) +
+  geom_col(aes(Period, impact_P), show.legend = FALSE) + 
+  xlab("Período de Avaliação") + ylab("Número de Publicações com Impacto") + 
+  geom_text(position = position_dodge(width = 0.9), vjust = -0.5) + theme_bw()
+ggplotly(plot3, tooltip = "text") %>% layout(showlegend = FALSE) %>% style(textposition = "top")
+
+
+plot4 <- brazil %>% 
+  filter(University=="UNIVERSIDADE FEDERAL DE VICOSA",
+         Field=="MATHEMATICS AND COMPUTER SCIENCE", 
+         Frac_counting=="1") %>% group_by(Period) %>% 
+  ggplot(aes(Period, impact_P, fill=Period, label= round(impact_P, digits = 2), 
+             text=paste("Produção:",impact_P, "<br>", 
+                        "Período:", Period))) +
+  geom_col(aes(Period, impact_P), show.legend = FALSE) + 
+  xlab("Período de Avaliação") + ylab("Número de Publicações com Impacto") + 
+  geom_text(position = position_dodge(width = 0.9), vjust = -0.5) + theme_bw()
+ggplotly(plot4, tooltip = "text") %>% layout(showlegend = FALSE) %>% style(textposition = "top")
+
+
+teste <- brazil %>% 
+  filter(University=="UNIVERSIDADE FEDERAL DE VICOSA",
+         Field=="MATHEMATICS AND COMPUTER SCIENCE", 
+         Frac_counting=="1") %>% group_by(Per_Init) 
